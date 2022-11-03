43fd371195b82cbdbe082f18d4a313c3357c1e2e
nciampelletti <natashat@shaw.ca>
2022-11-02 22:40:48 -0700

    work on image loading optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import "../styles/globals.css"
 // import "mapbox-gl/dist/mapbox-gl.css"
 import Layout from "../components/layout/Layout"
+import LoadingScreen from "../components/ui/LoadingScreen"
+import React, { useState, useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000)
+  }, [])
+
+  {
+    loading && <LoadingScreen />
+  }
+
   return (
     <Layout>
       <Component {...pageProps} />
